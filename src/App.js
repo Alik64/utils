e14666ac80448e3a47053385b57a6518c80e439b
Alik64 <albert.barsamov@gmail.com>
2022-05-17 15:51:19 +0200

    filesPreview + proptypes + classnames

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FileUploader from "./components/FileUploader";
 function App() {
   return (
     <div className="App">
-      <FileUploader />
+      <FileUploader multiple />
     </div>
   );
 }
--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -1,14 +1,21 @@
-import s from "./FileUploader.module.scss";
+import { useState } from "react";
 import { getBase64 } from "../../utils/getBase64";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import s from "./FileUploader.module.scss";
 
-const FileUploader = () => {
+const FileUploader = ({ multiple }) => {
+  const [files, setFiles] = useState([]);
+  console.log("files", files);
   const handleChange = (e) => {
     const filesList = e.target.files;
 
     for (const file of filesList) {
       console.log(file);
       getBase64(file).then((fileAsBase64) => {
-        console.log("fileAsbase64", fileAsBase64);
+        setFiles((prevState) => {
+          return [...prevState, fileAsBase64];
+        });
       });
     }
   };
@@ -22,17 +29,28 @@ const FileUploader = () => {
             type="file"
             className={s.uploader__inputFile}
             onChange={handleChange}
-            multiple
+            multiple={multiple}
           />
         </label>
-        <div className={s.uploader__previewFilesContainer}>
-          <ul className={s.uploader__previewFilesContainer_previewList}>
-            <li>Text</li>
+        <div className={s.previewFilesContainer}>
+          <ul className={s.previewList}>
+            {files.map((item, index) => (
+              <li key={index} className={cn(s.previewListItem)}>
+                <img className={s.previewIco} src={item} alt={item.name} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
   );
+};
+
+FileUploader.defaultProps = {
+  multiple: false,
+};
+FileUploader.propTypes = {
+  multiple: PropTypes.bool,
 };
 
 export default FileUploader;
