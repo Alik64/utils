9b398de7d47149baf34f7d3137a971a96c58b6b8
Alik64 <albert.barsamov@gmail.com>
2022-05-17 22:18:32 +0200

    add updateFileList

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -6,17 +6,38 @@ import s from "./FileUploader.module.scss";
 
 const FileUploader = ({ multiple }) => {
   const [files, setFiles] = useState([]);
+  const updateFileList = (filePayload) => {
+    setFiles((prevState) => {
+      return [...prevState, filePayload];
+    });
+  };
   console.log("files", files);
   const handleChange = (e) => {
     const filesList = e.target.files;
 
     for (const file of filesList) {
       console.log(file);
-      getBase64(file).then((fileAsBase64) => {
-        setFiles((prevState) => {
-          return [...prevState, fileAsBase64];
+      getBase64(file)
+        .then((fileAsBase64) => {
+          updateFileList({
+            file,
+            name: file.name,
+            type: file.type,
+            imgUrl: fileAsBase64,
+            status: "OK",
+            isLoading: false,
+          });
+        })
+        .catch((error) => {
+          updateFileList({
+            file,
+            name: file.name,
+            type: file.type,
+            imgUrl: null,
+            status: "ERROR",
+            isLoading: false,
+          });
         });
-      });
     }
   };
   return (
@@ -24,7 +45,7 @@ const FileUploader = ({ multiple }) => {
       <div className={s.uploader}>
         <h1 className={s.uploader__title}>Upload file</h1>
         <label className={s.uploader__dropZone}>
-          <span className={s.uploader__dropZone_ico}>🚀</span>
+          <span className={s.uploader__dropZone_ico}>💾</span>
           <input
             type="file"
             className={s.uploader__inputFile}
@@ -35,8 +56,12 @@ const FileUploader = ({ multiple }) => {
         <div className={s.previewFilesContainer}>
           <ul className={s.previewList}>
             {files.map((item, index) => (
-              <li key={index} className={cn(s.previewListItem)}>
-                <img className={s.previewIco} src={item} alt={item.name} />
+              <li key={index} className={cn(s.previewList__item)}>
+                <img
+                  className={s.previewList__ico}
+                  src={item.imgUrl}
+                  alt={item.name}
+                />
               </li>
             ))}
           </ul>
